f53c83bc8d4f01c3e0aa3a69943344220afbe9f0
Abdullah ALmeshekah <meshekah@MacBook-Pro.local>
2021-01-20 12:40:45 +0300

    frist Store

--- src/App.js
+++ src/App.js
@@ -4,19 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <title>Abdullah Almeshekah</title>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My Book Store</h1>
+        <h2>Think before you speak. Read before you think</h2>
+        <img src="book.png" alt="hbhk" />
+        
+        
+        
       </header>
     </div>
   );
